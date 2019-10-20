e1b11cc2a4b27d08bde4e9eae68459edc6a9c633
Tetsu Haruyama <tetsu.yes@gmail.com>
2019-10-20 13:36:31 +0900

    updated file names

--- init.lua
+++ init.lua
@@ -1,17 +1,11 @@
 --###### Highlighting for Karabiner-Elements ######
-require("Borders_for_KE")
+require("Indicator_KE")
 
 --###### Resizing, moving and snapping Windows ######
 require("Win_Management")
 
 --###### Alert Messages ######
 require("hs.alert")
---hs.alert.defaultStyle.atScreenEdge = 2
---hs.alert.defaultStyle.fillColor = { white = 0, alpha = 0.1 }
---hs.alert.defaultStyle.textSize = 15
---hs.alert.defaultStyle.strokeColor = { white = 1, alpha = 1 }
---hs.alert.defaultStyle.textColor = { white = 1, alpha = 1 }
---hs.alert.defaultStyle.textFont  = ".AppleSystemUIFont"
 
 --##### Reload config #####
 hs.hotkey.bind({"ctrl", "alt", "shift"}, 'R', function()
